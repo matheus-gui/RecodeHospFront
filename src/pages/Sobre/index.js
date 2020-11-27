3f6e5f1fb88ce6a352a42189801ce3f5aa5064dd
DESKTOP-KDPGONO\Pablo Henrique <pablo.brito@live.com>
2020-11-27 19:09:25 -0300

    commit sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Page from '../../components/Page';
+import './index.css';
 
 export default function index() {
     return (
         <Page title="Homepage">
-            Sobre
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+            <p>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
+            <p>in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+            <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+            <p>mollit anim id est laborum.</p>
+
         </Page>
     );
 }
