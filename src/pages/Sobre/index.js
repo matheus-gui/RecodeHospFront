7095d42c3d3b09bf0b2dc1d3d7b0568502aabbd4
DESKTOP-KDPGONO\Pablo Henrique <pablo.brito@live.com>
2020-11-27 19:15:20 -0300

    commit sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -5,13 +5,14 @@ import './index.css';
 export default function index() {
     return (
         <Page title="Homepage">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-            <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
-            <p>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
-            <p>in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
-            <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-            <p>mollit anim id est laborum.</p>
+            <h3>Lorem Ipsum</h3><br></br>
+            <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            <p className="p">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className="p"> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+            <p className="p">nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
+            <p className="p">in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+            <p className="p">sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+            <p className="p">mollit anim id est laborum.</p>
 
         </Page>
     );
